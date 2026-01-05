8210e894a7473141d15ae5ba8509460f511c626f
Prajakta Kharat <prajaktakharat456@gmail.com>
2026-01-05 23:17:52 +0530

    Edit file after previous commit

--- api_data_extraction.py
+++ api_data_extraction.py
@@ -7,7 +7,7 @@ import os
 # CONFIGURE THESE
 # -------------------------------
 API_URL = "https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/40381/hscard"
-API_KEY = "72e1bd568fmshd84b1b4352c0d19p134fe2jsndee496cdf794"
+API_KEY = "MY_API_KEY"
 API_HOST = "cricbuzz-cricket.p.rapidapi.com"
 OUTPUT_FOLDER = "output"
 # -------------------------------
